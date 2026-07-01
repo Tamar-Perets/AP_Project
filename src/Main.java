7a7b4048473bf97f6e444b34814927447168491d
Tamar-Perets <limudim.tamar@gmail.com>
2026-07-01 22:45:42 +0300

    main file

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import servlets.HtmlLoader;
+import server.MyHTTPServer;
+import server.HTTPServer;
+
+// this class operate the server etc.
+// to access the server, access from browser to: http://localhost:8080/app/index.html
+// to stop, enter anything in the console here.
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Initialize your server (assuming MyHTTPServer is your implementation)
+        HTTPServer server = new MyHTTPServer(8080, 5); 
+
+        // Add the HtmlLoader Servlet
+        server.addServlet("GET", "/app/", new HtmlLoader("html_files"));
+
+        server.start();
+        System.out.println("Server is running. Press Enter to stop.");
+        System.in.read();
+        server.close();
+        System.out.println("done");
+    }
+}
