7515463e45831120eb11d22ea29f510c747a3688
OfekSharon1 <135542208+OfekSharon1@users.noreply.github.com>
2026-07-03 17:07:32 +0300

    Update Topic.java

--- src/graph/Topic.java
+++ src/graph/Topic.java
@@ -1,55 +1,60 @@
-package graph; // TODO: for project, change package name to package graph; ?
+package graph;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Topic {
-	public final String name;
-	List<Agent> subs;
-	List<Agent> pubs;
-	
-	// constructor (privacy: default (package level only))
-	public Topic(String name) {
-		this.name = name;
-		this.subs = new CopyOnWriteArrayList<>();
-		this.pubs = new CopyOnWriteArrayList<>();
-	}
-	
-	// Getters
-	public List<Agent> getSubs() {
-	    return java.util.Collections.unmodifiableList(this.subs);
-	}
+    public final String name;
+    List<Agent> subs;
+    List<Agent> pubs;
 
-	public List<Agent> getPubs() {
-	    return java.util.Collections.unmodifiableList(this.pubs);
-	}
-	
-	// Add agent to the subscribers list
-	public void subscribe(Agent a) {
-		if (!this.subs.contains(a))
-			this.subs.add(a);
-	}
-	
-	// Remove agent from the subscribers list
-	public void unsubscribe(Agent a) { 
-		this.subs.remove(a);
-	}
-	
-	// Activate callback methods of all subs
-	public void publish(Message msg) {
-		for(int i=0; i<this.subs.size(); i++)
-			this.subs.get(i).callback(this.name, msg);
-	}
-	
-	// Add agent to the publishers list
-	public void addPublisher(Agent a) {
-		if (!this.pubs.contains(a))
-			this.pubs.add(a);
-	}
-	
-	// Remove agent from the publishers list
-	public void removePublisher(Agent a) { 
-		this.pubs.remove(a);
-	} 
+    private Message lastMessage;
 
+    public Topic(String name) {
+        this.name = name;
+        this.subs = new CopyOnWriteArrayList<>();
+        this.pubs = new CopyOnWriteArrayList<>();
+        this.lastMessage = null;
+    }
+
+    public List<Agent> getSubs() {
+        return Collections.unmodifiableList(this.subs);
+    }
+
+    public List<Agent> getPubs() {
+        return Collections.unmodifiableList(this.pubs);
+    }
+
+    public Message getLastMessage() {
+        return lastMessage;
+    }
+
+    public void subscribe(Agent a) {
+        if (!this.subs.contains(a)) {
+            this.subs.add(a);
+        }
+    }
+
+    public void unsubscribe(Agent a) {
+        this.subs.remove(a);
+    }
+
+    public void publish(Message msg) {
+        this.lastMessage = msg;
+
+        for (Agent sub : this.subs) {
+            sub.callback(this.name, msg);
+        }
+    }
+
+    public void addPublisher(Agent a) {
+        if (!this.pubs.contains(a)) {
+            this.pubs.add(a);
+        }
+    }
+
+    public void removePublisher(Agent a) {
+        this.pubs.remove(a);
+    }
 }
